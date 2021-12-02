b281f62d3c0f82a856aada18ce9dec2f15adcf00
nascott <nascott94@outlook.com>
2021-12-02 14:24:53 -0700

    set up intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { Route, Switch, useLocation } from "react-router";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
 import GlobalStyle from "./globalStyles";
 
+//components
+import { Main } from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
+import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar";
+import { Intro } from "./components/Intro";
+
 function App() {
+  const location = useLocation();
+
   return (
-    <div>
+    <>
       <GlobalStyle />
-      App File
-    </div>
+      <Intro />
+
+      <ThemeProvider theme={lightTheme}></ThemeProvider>
+    </>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,26 @@ import React from "react";
 import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
+import { Intro } from "./Intro";
+
+const MainContainer = styled.div`
+  background: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
+  position: relative;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+    font-weight: 500;
+  }
+`;
 
 export const Main = () => {
-  return <div></div>;
+  return <MainContainer></MainContainer>;
 };
